e05f09a68c8b6a4cd80c74ff24f815773a117697
d0c0b <d0c0b@outlook.com>
2016-04-02 13:10:10 +0900

    modify bot name

--- bin/hubot.cmd
+++ bin/hubot.cmd
@@ -1,5 +1,3 @@
 @echo off
 
-npm install && node_modules\.bin\hubot.cmd --name "bot" %* 
-
-set HUBOT_SLACK_TOKEN=xoxb-31478083361-NjtlceGdPAKu3XdyY0ksajHg
+npm install && node_modules\.bin\hubot.cmd --name "bot" %*
